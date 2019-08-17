c0e1fb7b1e852d1f900e2b8e0ce70cd40e54c861
MrT3313 <rturgeon13@gmail.com>
2019-08-17 14:48:54 -0500

    locationsRoute.js -> Get/

--- api/routes/locationsRoute.js
+++ api/routes/locationsRoute.js
@@ -10,12 +10,40 @@ const express = require('express')
 // MIDDLEWARE
 
 // - GET - //
-    router.get('/', async(req,res) => {
-        console.log('locationsRouter get/')
-        res.status(200).json({ message: 'GET/ --> in locationsRouter'})
+    router.get('/', async(req, res) => {
+        // -- TEST -- //
+            console.log('locationsRouter get/')
+            // res.status(200).json({ message: 'GET/ --> in locationsRouter'})
+        // -- //
+            DB_KNEX('locations')
+                .then( ALL_locations => {
+                    console.log('ALL_locations', ALL_locations)
+                    res.status(200).json( ALL_locations )
+                })
+                .catch( err => {
+                    res.status(500).json( {error: 'Unable to Get Locations'} )
+                })
+        
     })
 
+    router.get('/:id', async(req,res) => {
+        console.log('locationsRouter get/')
+        res.status(200).json({ message: 'GET/:id --> in locationsRouter'})
+    })
+
+
 // - POST - //
+    /* Accepted SHape
+        {
+
+        } 
+    */
+
+    router.post('/', async(req,res) => {
+        console.log('locationRouter post/')
+        
+    })
+
 // - PUT - //
 // - DEL - //
 
